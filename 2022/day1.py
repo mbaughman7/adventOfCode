7b56c6c16bcbc46a9175b48a40d3b208b662e51d
Marshall Baughman <95940057+mbaughman7@users.noreply.github.com>
2022-12-01 10:29:29 -0600

    wrapped part two into function

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,10 +1,9 @@
-with open("test_data.txt") as myFile:
+with open("test_data.txt") as myFile:  #change this to be done automatically
     raw_array = myFile.readlines()
 
 adder = 0
 counter = 0
 snack_dict = {}
-top_three_total = 0
 
 for item in raw_array:
     if(item != '\n'):
@@ -27,20 +26,14 @@ print(highest_cal)
 #########################
 # for part 2, add the top calorie count to top_three_total, delete it from the dict,
 # then find next highest, add it, delete it from dict, and so on.
-top_three_total += highest_cal
+def addTopSpots(num):
+    top_three_total = 0
+    for i in range(num):
+        highest_elf = max(snack_dict, key=snack_dict.get)
+        high_cal = max(snack_dict.values())
+        top_three_total += high_cal
+        del snack_dict[highest_elf]
+    print(top_three_total)
 
-del snack_dict[highest_cal_elf]
-
-second_highest_elf = max(snack_dict, key=snack_dict.get)
-second_highest_cal = max(snack_dict.values())
-
-top_three_total += second_highest_cal
-del snack_dict[second_highest_elf]
-
-third_highest_elf = max(snack_dict, key=snack_dict.get)
-third_highest_cal = max(snack_dict.values())
-
-top_three_total += third_highest_cal
-
-print(top_three_total)
+addTopSpots(3)
 
